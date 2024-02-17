051c30617adf2d34ca2fecdd405ffb309e116daa
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-02-17 14:20:07 -0800

    figured out how to get the color on the image

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <main className="min-w-svw min-h-svh flex justify-center items-center bg-[--very-dark-blue]">
       <div className="flex flex-col w-80 justify-center items-center bg-[--dark-desaturated-blue] text-[--slightly-transparent-white] rounded-lg">
-        <img src="../images/image-header-mobile.jpg"></img>
+        <div className="w-full h-60 rounded-t-md bg-[--soft-violet] mix-blend-multiply"></div>
+        <img
+          className="-mt-60 rounded-t-md"
+          src="../images/image-header-mobile.jpg"
+        />
         <h1> Get insights that help your business grow.</h1>
         <p>
           Discover the benefits of data analytics and make better decisions
